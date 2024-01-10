0e5d105550f13ef2f5ae5d1d37d35941b370c1ef
Jackliu-miaozi <71253778+Jackliu-miaozi@users.noreply.github.com>
2024-01-10 16:37:33 +0800

    在mock中增加了注释

--- pallets/kitties/src/mock.rs
+++ pallets/kitties/src/mock.rs
@@ -2,7 +2,7 @@
 use crate as pallet_kitties;
 //为当前crate引用一个别名，crate用于引用当前crate。
 //as 用来创建别名。
-use frame_support::traits::{ConstU16, ConstU64};
+use frame_support::traits::{ConstU16, ConstU64， ConstU32};
 //引入ConstU16和ConstU64,这两个是常量，ConstU16是一个u16类型的常量，ConstU64是一个u64类型的常量。
 use pallet_insecure_randomness_collective_flip;
 //pallet支持热插拔，这行代码引入了一个pallet他是一个不安全的随机数生成器。
@@ -50,6 +50,11 @@ frame_support::construct_runtime!(
 		//=左边是一个固定的关键字
 		UncheckedExtrinsic = UncheckedExtrinsic,
 		//这是定义未经检查的外部交易类型的地方。
+		//Block：用于指定运行时的区块类型。
+		// NodeBlock：用于指定节点的区块类型。在大多数情况下，NodeBlock 的类型和 Block 的类型是一样的。
+		// UncheckedExtrinsic：用于指定运行时的未经检查的外部交易类型。未经检查的外部交易是指还没有经过签名验证和交易费支付检查的交易。
+		//还有其他很多关键字
+		//没有指定的关键字类型会被约束为默认的类型。
 	{
 		System: frame_system,
 		//这是在运行时中添加 frame_system pallet 的地方。
@@ -66,42 +71,91 @@ frame_support::construct_runtime!(
 
 impl frame_system::Config for Test {
 	type BaseCallFilter = frame_support::traits::Everything;
+	//用于过滤调用的类型。这里设置为 Everything，表示所有的调用都会被接受。
 	type BlockWeights = ();
+	//用于定义区块的权重和长度。这里设置为 ()，表示使用默认的权重和长度。
 	type BlockLength = ();
+	//用于定义区块的权重和长度。这里设置为 ()，表示使用默认的权重和长度。
 	type DbWeight = ();
+	//用于定义数据库操作的权重。这里设置为 ()，表示使用默认的权重。
 	type RuntimeOrigin = RuntimeOrigin;
+	//todo 为什么=右边是同名的，但是在别的地方还找不到声明？
 	type RuntimeCall = RuntimeCall;
+	//todo 为什么=右边是同名的，但是在别的地方还找不到声明？
 	type Index = u64;
+	//用于定义账户的索引类型。这里设置为 u64，表示账户的索引类型是 u64。
 	type BlockNumber = u64;
+	//用于定义区块号的类型。这里设置为 u64，表示区块号的类型是 u64。
 	type Hash = H256;
+	//用于定义哈希值的类型。这里设置为 H256，表示哈希值的类型是 H256。
 	type Hashing = BlakeTwo256;
+	//用于定义哈希算法的类型。这里设置为 BlakeTwo256，表示哈希算法的类型是 BlakeTwo256。
 	type AccountId = u64;
+	//用于定义账户的类型。这里设置为 u64，表示账户的类型是 u64。
 	type Lookup = IdentityLookup<Self::AccountId>;
+	//用于定义账户查找的类型。这里设置为 IdentityLookup<Self::AccountId>，表示账户查找的类型是 IdentityLookup<Self::AccountId>。
 	type Header = Header;
+	//用于定义区块头的类型。这里设置为 Header，表示区块头的类型是 Header。
 	type RuntimeEvent = RuntimeEvent;
+	//用于定义运行时事件的类型。这里设置为 RuntimeEvent，表示运行时事件的类型是 RuntimeEvent。
+	//这个runtimeevent是在pallet_kitties中定义的。
 	type BlockHashCount = ConstU64<250>;
+	//用于定义区块哈希数量的类型。这里设置为 ConstU64<250>，表示区块哈希数量的类型是 ConstU64<250>。
+	//它定义了区块链保存的最近区块哈希的数量。
+	//ConstU64<250> 是一个常量类型，它表示一个固定的 u64 值，这里值是250。
+	//这行代码的意思是，区块链将保存最近的 250 个区块的哈希。这是为了在需要时可以查询这些区块的哈希，例如在验证交易时。但是，超过这个数量的旧区块哈希将被丢弃，以节省状态数据库的存储空间。
+	//这并不影响区块数据本身，区块数据通常会被完整地保存在区块链节点的本地存储中。只是你无法再通过哈希直接从状态数据库中查询到这些旧的区块。
 	type Version = ();
+	//用于定义运行时版本的类型。这里设置为 ()，表示运行时版本的类型是 ()。
 	type PalletInfo = PalletInfo;
+	//用于定义 pallet 信息的类型。这里设置为 PalletInfo，表示 pallet 信息的类型是 PalletInfo。
+	//PalletInfo 是一个 trait，它定义了一些用于获取 pallet 信息的方法。
+	//todo 在lib.rs和mock.rs中都找不到PalletInfo的定义
+
 	type AccountData = ();
+	//用于定义账户数据的类型。这里设置为 ()，表示使用默认的帐户数据类型。
 	type OnNewAccount = ();
+	//用于定义新账户创建时的操作。这里设置为 ()，表示不执行任何操作。
 	type OnKilledAccount = ();
+	//用于定义账户被消除时的操作。这里设置为 ()，表示不执行任何操作。
 	type SystemWeightInfo = ();
+	//用于定义系统权重信息的类型。这里设置为 ()，表示使用默认的系统权重信息类型。
 	type SS58Prefix = ConstU16<42>;
+	//用于定义 SS58 编码的前缀。这里设置为 ConstU16<42>，表示 SS58 编码的前缀是 42。
 	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+	//用于定义设置代码时的操作。这里设置为 ()，表示不执行任何操作。
+	type MaxConsumers = ConstU32<16>;
+	//用于定义最大消费者数量的类型。这里设置为 ConstU32<16>，表示最大消费者数量的类型是 ConstU32<16>。
+	//使用ConstU32<16>类型，的好处是，在编译时ConstU32 可以作为类型参数，而 16 不能。
+	//在 Rust 中，类型参数必须是类型，而不能是值。
 }
 
 impl pallet_kitties::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
+	//用于定义运行时事件的类型。这里设置为 RuntimeEvent，表示运行时事件的类型是 RuntimeEvent。
+	//这个runtimeevent是在pallet_kitties中定义的。
 	type Randomness = Randomness;
+	//用于定义随机数生成器的类型。这里设置为 Randomness，表示随机数生成器的类型是 Randomness。
 	type Currency: Currency<Self::AccountId>;
+	//用于定义货币类型。这里设置为 Currency<Self::AccountId>，表示货币类型是 Currency<Self::AccountId>。
 }
-
+//这里为pallet_kitties的配置进行具体的实现
+//这里的= 左边的Randomness和Currency是在pallet_kitties中定义的。
+//在这里进行具体的实现。
 impl pallet_insecure_randomness_collective_flip::Config for Test {}
+//这个pallet没有具体的关联类型，所以这里是空。
+//虽然是关联类型是空，但是也需要把它实现给Test。这个虚拟的runtime。
 
 pub fn new_test_ext() -> sp_io::TestExternalities {
+	//sp_io::TestExternalities 是 Substrate 中用于模拟区块链环境的一个类型，
+	//它可以让你在测试中模拟执行区块链交易
 	let mut ext: sp_io::TestExternalities =
 		frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
+	//这里创建了一个新的TestExternalities
+	//这个实例包含了默认的创世区块配置，并且被转换为 TestExternalities 类型
 	ext.execute_with(|| System::set_block_number(1));
+	//设置初始的区块号为 1。execute_with 方法允许你在 TestExternalities 的环境中执行一个闭包。
+	//这个闭包更改了ext
+	//所以这里的ext是一个可变的引用
 	ext
 }
